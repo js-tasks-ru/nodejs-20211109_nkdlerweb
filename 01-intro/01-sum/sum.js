5d88c8d5056bb8bf10558c42e961e1014a75c8ef
SchHeinz <nkdlerweb@gmail.com>
2021-11-14 17:06:49 +0200

    1st task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
-  /* ваш код */
+
+    if ([a, b].some((value) => typeof value !== 'number')) {
+        throw new TypeError();
+    }
+
+    return a + b;
 }
 
 module.exports = sum;
